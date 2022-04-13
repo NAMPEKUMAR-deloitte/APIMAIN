f627465058f578600b5df73a3799f2408492cf79
Kumar <nampekumar@deloitte.com>
2022-04-13 13:10:41 +0530

    mainassignment

---
+++ src/test/java/testcase1/AddTaskTest.java
@@ -0,0 +1,87 @@
+package testcase1;
+
+import userdb1.TasksDB;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class AddTaskTest {
+
+    RequestSpecification requestSpec;
+    ResponseSpecification responseSpec;
+
+
+    @BeforeClass
+    public void setUp()
+    {
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
+        requestSpecBuilder.setBaseUri("https://gorest.co.in/public/v1/users").
+                addHeader("Content-Type","application/json");
+
+        requestSpec = RestAssured.with().spec(requestSpecBuilder.build());
+
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder().
+                expectContentType(ContentType.JSON);
+        responseSpec=responseSpecBuilder.build();
+    }
+
+    @Test
+    public void addTaskTest() throws Exception {
+        UserDetails user= new UserDetails();
+        TasksDB tasksDB = new TasksDB();
+        ArrayList list;
+        HashMap hashMap;
+        hashMap=user.registerUser();
+        String checkToken = (String) hashMap.get("token");
+        list =tasksDB.addTasks();
+        for(int i=0;i< list.size();i++)
+        {
+            HashMap hash = new HashMap<>();
+            hash.put("description",list.get(i));
+            Response response = requestSpec.body(hash).auth().oauth2(checkToken).request(Method.POST,"/task");
+            String check = response.getBody().asString();
+            System.out.println(response.getStatusCode());
+            JSONObject obj = new JSONObject(response.asString());
+            System.out.println(obj);
+            JSONArray array = (JSONArray)obj.get("data");
+            System.out.println(array);
+            JSONObject ownerObj = array.getJSONObject(i);
+            String ownerStr = ownerObj.getString("owner");
+            System.out.println(ownerStr);
+        }
+
+
+        System.out.println(checkToken);
+        System.out.println(list);
+    }
+
+    @Test
+    public void getOwnerIDTest() throws Exception {
+        UserDetails user =new UserDetails();
+        TasksDB tasksDB = new TasksDB();
+        HashMap hashmap;
+        hashmap= user.registerUser();
+        String token = (String) hashmap.get("token");
+        Response response =requestSpec.auth().oauth2(token).request(Method.GET,"/task");
+        JSONObject object = new JSONObject(response.asString());
+        JSONArray array = (JSONArray)object.get("data");
+        JSONObject object1 = array.getJSONObject(0);
+
+
+        System.out.println(array);
+        System.out.println(object1.get("description"));
+        System.out.println(token);
+    }
+}
